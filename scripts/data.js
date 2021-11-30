7b244cddd42d1583d971f82b2086c30dcd2650c3
Tanmay Goyal <tanmay.goyal.pro@gmail.com>
2021-11-30 23:12:11 +0530

    Added Project
    Added Platform

---
+++ scripts/data.js
@@ -0,0 +1,221 @@
+var stages = {
+    stage1: {
+        data: [{
+                "question": "Photoelectric effect is associated with?",
+                "options": [
+                    "A: Non-Metals",
+                    "B: Gases",
+                    "C: Metals",
+                    "D: Photosynthesis",
+                ],
+                "ans": "C"
+            },
+            {
+                "question": "Which of these is not a JavaScript library?",
+                "options": [
+                    "A: jQuery",
+                    "B: Angular",
+                    "C: React",
+                    "D: Immutablejs",
+                ],
+                "ans": "B"
+            },
+            {
+                "question": "Which is a CSS framework?",
+                "options": [
+                    "A: Popper",
+                    "B: Modernizr",
+                    "C: Bootstrap",
+                    "D: Sass",
+                ],
+                "ans": "C"
+            },
+            {
+                "question": "Which language is closer to the hardware?",
+                "options": [
+                    "A: Python",
+                    "B: Hash",
+                    "C: C++",
+                    "D: JavaScript",
+                ],
+                "ans": "C"
+            },
+            {
+                "question": "Who is the creator of Python?",
+                "options": [
+                    "A: Guido van Rossium",
+                    "B: Brendan Eich",
+                    "C: Google Inc",
+                    "D: Dan Abrahmov",
+                ],
+                "ans": "A"
+            },
+            {
+                "question": "Colourless and odourless is the property of?",
+                "options": [
+                    "A: Calcium",
+                    "B: Ammonium",
+                    "C: Chlorine",
+                    "D: Methane",
+                ],
+                "ans": "D"
+            },
+            {
+                "question": "Who created the C programming language?",
+                "options": [
+                    "A: Ken Jones",
+                    "B: Dennis Ritchie",
+                    "C: Bjarnne Stautroup",
+                    "D: Tom Smith",
+                ],
+                "ans": "B"
+            }
+        ]
+    },
+    stage2: {
+        data: [{
+                "question": "What is the full meaning of PHP?",
+                "options": [
+                    "A: Hypertext Preprocessor",
+                    "B: Pre Hypertext Processor",
+                    "C: Page High Power",
+                    "D: Power High Page",
+                ],
+                "ans": "A"
+            },
+            {
+                "question": "In JavaScript which is an abstract data type?",
+                "options": [
+                    "A: Array",
+                    "B: List",
+                    "C: Symbol",
+                    "D: String",
+                ],
+                "ans": "B"
+            },
+            {
+                "question": "Which is a PHP framework?",
+                "options": [
+                    "A: Composer",
+                    "B: WebGl",
+                    "C: Mongoose",
+                    "D: Laravel",
+                ],
+                "ans": "D"
+            },
+            {
+                "question": "Which is the largest country by population?",
+                "options": [
+                    "A: Russia",
+                    "B: United States of America",
+                    "C: China",
+                    "D: Japan",
+                ],
+                "ans": "C"
+            },
+            {
+                "question": "Flourine element is known as?",
+                "options": [
+                    "A: Halogen",
+                    "B: Ester",
+                    "C: Rare gas",
+                    "D: Ethanol",
+                ],
+                "ans": "A"
+            },
+            {
+                "question": "Which of these is not a framework for building Android application?",
+                "options": [
+                    "A: React Native",
+                    "B: Ionic",
+                    "C: Phonegap",
+                    "D: Kotlin",
+                ],
+                "ans": "D"
+            },
+            {
+                "question": "Which of these is not a metal?",
+                "options": [
+                    "A: Calcium",
+                    "B: Aluminium",
+                    "C: Argon",
+                    "D: Sodium",
+                ],
+                "ans": "C"
+            }
+        ]
+    },
+    stage3: {
+        data: [{
+                "question": "Chai is a tool used for?",
+                "options": [
+                    "A: Testing",
+                    "B: Module bundling",
+                    "C: Designing",
+                    "D: Data Science",
+                ],
+                "ans": "A"
+            },
+            {
+                "question": "Which is not used for module bundling and task running?",
+                "options": [
+                    "A: Webpack",
+                    "B: Mocha",
+                    "C: Gulp",
+                    "D: Grunt",
+                ],
+                "ans": "B"
+            },
+            {
+                "question": "Tkinter is a python library used for?",
+                "options": [
+                    "A: Artificial Intelligence",
+                    "B: Machine Learning",
+                    "C: Desktop development",
+                    "D: Mobile development",
+                ],
+                "ans": "C"
+            },
+            {
+                "question": "Golang was created at?",
+                "options": [
+                    "A: Facebook",
+                    "B: Zeit",
+                    "C: Microsoft",
+                    "D: Google",
+                ],
+                "ans": "D"
+            },
+            {
+                "question": "Scrapy is a framework used for?",
+                "options": [
+                    "A: Web crawling",
+                    "B: Game development",
+                    "C: Network sniffing",
+                    "D: Web development",
+                ],
+                "ans": "A"
+            },
+            {
+                "question": "Nodejs is usually known for its?",
+                "options": [
+                    "A: Speed",
+                    "B: Efficiency",
+                    "C: Simplicity",
+                    "D: Asyncronousity",
+                ],
+                "ans": "D"
+            },
+            {
+                "question": "The largest planet is?",
+                "options": [
+                    "A: Saturn",
+                    "B: Jupiter",
+                    "C: Earth",
+                    "D: Uranium",
+                ],
+                "ans": "B"
+            }
+        ]
+    }
+}
---
+++ scripts/main.js
@@ -0,0 +1,353 @@
+let stageLen = 1,
+    taken = [],
+    c = 2,
+    gameLength = $(".stages button").length,
+    pLen = gameLength,
+    amount = $(".stages button").eq(pLen - 1).html(),
+    audience = true,
+    call_a_friend = true,
+    fifty_fifty = true,
+    play = true,
+    selected,
+    answer
+
+/**
+ * The millionaire game object
+ */
+let millionaire = {
+    /**
+     * Returns a random number from 1 to 6, which is used 
+     * for selection of game data. This function also makes 
+     * sure a number is not selected twice.
+     */
+    random: function() {
+        var rand = Math.floor(Math.random() * 7)
+        try {
+            while (taken.toString().match(rand)) {
+                rand = Math.floor(Math.random() * 7);
+            }
+            taken.push(rand);
+            return rand;
+        } catch (e) {}
+    },
+
+
+    /**
+     * Choose question from data
+     */
+    selectQuestion: function() {
+        var span, rand
+        rand = this.random()
+
+        // Change stage data at every stage length of 6
+        if (stageLen < 6) {
+            $('.question').html(stages.stage1.data[rand]["question"])
+            stages.stage1.data[rand]["options"].map((option, i) => {
+                span = `<span class="${option.slice(0, 1)} opt" key="${i}">${option}</span>`
+                $(".options").append(span)
+            })
+
+            $(".opt").click((e) => {
+                selected = e.target.textContent.slice(0, 1)
+                $(".modal").fadeIn(300);
+                $(".warning").fadeIn(300)
+            })
+
+            answer = stages.stage1.data[rand]["ans"]
+        } else if (stageLen < 12) {
+            $('.question').html(stages.stage2.data[rand]["question"])
+            stages.stage2.data[rand]["options"].map((option, i) => {
+                span = `<span class="${option.slice(0, 1)} opt" key="${i}">${option}</span>`
+                $(".options").append(span)
+            })
+
+            $(".opt").click((e) => {
+                selected = e.target.textContent.slice(0, 1)
+                $(".modal").fadeIn(300);
+                $(".warning").fadeIn(300)
+            })
+
+            answer = stages.stage2.data[rand]["ans"]
+        } else {
+            $('.question').html(stages.stage3.data[rand]["question"])
+            stages.stage3.data[rand]["options"].map((option, i) => {
+                span = `<span class="${option.slice(0, 1)} opt" key="${i}">${option}</span>`
+                $(".options").append(span)
+            })
+
+            $(".opt").click((e) => {
+                selected = e.target.textContent.slice(0, 1)
+                $(".modal").fadeIn(300);
+                $(".warning").fadeIn(300)
+            })
+
+            answer = stages.stage3.data[rand]["ans"]
+        }
+    },
+
+
+    /**
+     * Start the game
+     */
+    start: function() {
+        $('.welcome').fadeOut(500, function() {
+            $('#game').fadeIn(500)
+        });
+        this.selectQuestion()
+    },
+
+
+    /**
+     * Show congratulation message
+     */
+    won: function() {
+        if (stageLen < 15) {
+            stageLen++
+            taken = (stageLen % 6 == 0) || (stageLen % 11 == 0) ? [] : taken;
+            amount = $(".stages button").eq(pLen - 1).html();
+
+            $('.right-wrapper').fadeIn(800);
+            setTimeout(function() { $('#right').html("Bilkul Sahi Jawab!!") }, 500)
+            setTimeout(function() { $('#right').html("Aapne Jeet liye hain " + amount + " points") }, 1800)
+            setTimeout(function() { $('#right').html("Aur ye raha agla question aapke computer screen par") }, 3800)
+            setTimeout(function() {
+                $('.right-wrapper').fadeOut(1500, function() {
+                    $('#right').html("")
+                    $(".modal").fadeOut()
+                })
+            }, 4500)
+
+            pLen--;
+            $(".current").removeClass("current");
+            $(".stages button").eq(pLen - 1).addClass("current");
+            $(".score").html("Score: " + amount)
+            gameLength++;
+
+            setTimeout(() => {
+                this.selectQuestion()
+            }, 4500)
+        } else {
+            $('.right-wrapper').fadeIn(800);
+            setTimeout(function() { $('#right').html("Bilkul Sahi Jawab!! ") }, 500)
+            setTimeout(function() { $('#right').html("Bohot Adhbhut khela hai aapne. Aap ban gaye hain crorepati!!") }, 1800)
+            setTimeout(() => {
+                $('.right-wrapper').fadeOut(800, () => {
+                    $('#right').html("")
+                    this.reset()
+                })
+            }, 2300)
+        }
+    },
+
+
+    /**
+     * Show loose message
+     */
+    loose: function() {
+        $('.wrong-wrapper').fadeIn(800);
+
+        setTimeout(
+            function() { $('#wrong').html(`Aapka uttar galat hai<br> Aap jeetein hain ${amount} points`) }, 100)
+
+        setTimeout(
+            function() {
+                $('.wrong-wrapper').fadeOut(function() {
+                    $('#wrong').html("")
+                    $(".modal").fadeOut(800)
+                })
+            }, 2000)
+
+        setTimeout(() => { this.reset() }, 2000)
+    },
+
+
+    /**
+     * Activate fifty fifty lifeline
+     */
+    fifty: function() {
+        var options = ["D", "A", "C", "B"]
+        var removedOptions = []
+        $(".fifty").attr({ "src": "../images/fifty2.png" }).css("cursor", "default")
+        $(".fifty:hover").css("background-color", "rgb(17, 17, 138)")
+        fifty_fifty = false
+
+        for (var i = 0; i < options.length; i++) {
+            if (options[i] != answer) {
+                if (removedOptions.length < 2) {
+                    removedOptions.push(options[i])
+                    for (var i = 0; i < removedOptions.length; i++) {
+                        $(`span.${removedOptions[i]}`).html(`${removedOptions[i]}:`)
+                    }
+                }
+            }
+        }
+    },
+
+
+    /**
+     * Activate call a friend
+     */
+    call: function() {
+        let randFriend = Math.floor(Math.random() * 7);
+        let randResp = Math.floor(Math.random() * 4)
+        let friend = ["Ngozi", "Ekene", "Chioma", "Kenneth", "Bright", "Chisom", "Adaeze"]
+        let sure = ["100%", "80%", "60%", "50%", "30%"]
+        let resp = ["Am sure it\'s", "It\'s certainly", "It\'s definitely", "I think it\'s", "Am not sure to choose"]
+
+        $(".call").attr({ "src": "../images/call2.png" }).css("cursor", "default")
+        $(".call:hover").css("background-color", "rgb(17, 17, 138)")
+        $(".modal").fadeIn()
+        $('.chat-wrapper').fadeIn(500);
+
+        setTimeout(() => {
+            $('#chat').html("Calling... ☎")
+        }, 100)
+
+        setTimeout(() => {
+            $('#chat').html("Connected ✔")
+        }, 2000)
+
+        setTimeout(
+            function() {
+                $('#chat').html(`ME: Hello ${friend[randFriend]}. 😞 Am in a hot seat now and I need the answer to this question.<br>${$('.question').html()}`)
+            }, 4800)
+
+        setTimeout(
+            function() {
+                $('#chat').html(`${friend[randFriend]}: Thinking.....`)
+            }, 8000)
+
+        setTimeout(
+            function() {
+                $('#chat').html(`${friend[randFriend]}: ${resp[randResp]} ${answer}.`)
+            }, 10000)
+
+        setTimeout(
+            function() {
+                $('#chat').html("ME: How sure are you?")
+            }, 13000)
+
+        setTimeout(
+            function() {
+                $('#chat').html(`${friend[randFriend]}: ${sure[randResp]} sure.`)
+            }, 16000)
+
+        setTimeout(
+            function() {
+                $('.chat-wrapper').fadeOut(800, function() {
+                    $('#chat').html("")
+                    $(".modal").fadeOut()
+                    call_a_friend = false
+                })
+            }, 18000)
+    },
+
+
+    /**
+     * Activate audience lifeline
+     */
+    audience: function() {
+        $(".aud").attr({ "src": "../images/aud2.png" }).css("cursor", "default")
+        $(".aud:hover").css("background-color", "rgb(17, 17, 138)")
+        $(".modal").fadeIn()
+        $(".chat-wrapper").fadeIn(500)
+
+        setTimeout(function() {
+            $('#chat').html("Throwing question to audience...")
+        }, 100)
+
+        setTimeout(function() {
+            $('#chat').html("Audience thinking...")
+        }, 1200)
+
+        setTimeout(function() {
+            $(".chat-wrapper").fadeOut()
+            $('#chat').html("")
+            $('.audience-wrapper').fadeIn()
+            audience = false
+        }, 2700)
+
+        var options = ["D", "A", "C", "B"]
+        var audiencePercentage = ["15", "32", "48", "54", "60"]
+
+        for (var i = 0; i < options.length; i++) {
+            if (options[i] != answer) {
+                $(`.bar-${options[i]}`).css("width", `${audiencePercentage[i]}%`)
+            } else {
+                var highestPercentage = Number(audiencePercentage[4]) +
+                    Math.floor(Math.random() * (23 - 10)) + 10
+                $(`.bar-${answer}`).css("width", `${highestPercentage}%`)
+            }
+        }
+
+        $(".closeBtn").click(function() {
+            $(".audience-wrapper").fadeOut()
+            $(".modal").fadeOut()
+        })
+    },
+
+
+    /**
+     * Reset game. This is called when you either finished with a win or lost
+     */
+    reset: function() {
+        $(".modal").fadeOut()
+        $("#game").fadeOut()
+
+        setTimeout(
+            function() { $(".welcome").fadeIn(600) }, 1000)
+
+        call_a_friend = true, audience = true, fifty_fifty = true
+        window.stageLen = 1;
+        taken = [];
+        c = 2
+
+        gameLength = $(".stages button").length
+        pLen = gameLength
+        amount = $(".stages button").eq(pLen - 1).html();
+
+        $("img:hover").css("background-color", "rgb(250, 121, 0) !important")
+        $(".fifty").attr({ "src": "../images/fifty.png", "onClick": "game.fifty()" }).css("cursor", "pointer")
+        $(".call_a_friend").attr({ "src": "../images/call.png", "onClick": "game.call_a_friend()" }).css("cursor", "pointer")
+    }
+}
+
+
+/**
+ * Mouse event handlers
+ */
+$(".no").click(function() {
+    $(".modal").fadeOut()
+});
+
+$(".yes").click(function() {
+    $('.warning').fadeOut(500,
+        function() {
+            (selected == answer) ? millionaire.won(): millionaire.loose()
+        }
+    )
+})
+
+$(".about-btn").click(function() {
+    $('.about').fadeIn(1000);
+})
+
+$(".fifty").click(function() {
+    if (fifty_fifty)
+        millionaire.fifty()
+})
+
+$(".call").click(function() {
+    if (call_a_friend)
+        millionaire.call()
+})
+
+$(".aud").click(function() {
+    if (audience)
+        millionaire.audience()
+})
+
+// $(".start-btn").click(function() {
+//     millionaire.start()
+// })
+setTimeout(() => { millionaire.start() }, 5000);
